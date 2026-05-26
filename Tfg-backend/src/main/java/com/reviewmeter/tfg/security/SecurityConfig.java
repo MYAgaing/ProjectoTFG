c47751a00c04687825b07038bc13bf85a71be273
MYAgaing <75101772+MYAgaing@users.noreply.github.com>
2026-05-26 14:22:30 +0200

    Cambios en el backend

--- Tfg-backend/src/main/java/com/reviewmeter/tfg/security/SecurityConfig.java
+++ Tfg-backend/src/main/java/com/reviewmeter/tfg/security/SecurityConfig.java
@@ -81,7 +81,8 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:4200",
                 "https://motivated-energy-production.up.railway.app",
-                "https://projectotfg-production.up.railway.app"
+                "https://projectotfg-production.up.railway.app",
+                "https://mellow-vibrancy-production.up.railway.app"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With", "Accept"));
--- Tfg-frontend/src/environments/environment.prod.ts
+++ Tfg-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://projectotfg-production.up.railway.app'
+  apiUrl: 'https://mellow-vibrancy-production.up.railway.app'
 };
